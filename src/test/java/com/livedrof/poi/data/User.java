a2e02a292e988bd80112e52828a4a213cbab61bf
congshuanglong <congshuanglong@126.com>
2018-04-29 00:14:11 +0800

    完成测试

---
+++ src/test/java/com/livedrof/poi/data/User.java
@@ -0,0 +1,31 @@
+package com.livedrof.poi.data;
+
+public class User {
+    private int id;
+    private String username;
+    private String telephone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
--- src/test/java/com/livedrof/poi/excel/ExporterTest.java
+++ src/test/java/com/livedrof/poi/excel/ExporterTest.java
@@ -1,4 +1,39 @@
 package com.livedrof.poi.excel;
 
+import com.livedrof.poi.data.User;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ExporterTest {
+    @Test
+    public void testUserExporter() throws IOException {
+        Exporter exporter = Exporter.getInstance();
+        DataSheet<User> userDataSheet = exporter.sheet("用户信息");
+        userDataSheet.getDataTable()
+                .addColumn("ID", "id")
+                .nextColumn("姓名", "username")
+                .nextColumn("手机号", "telephone");
+        userDataSheet.setData(this.getData());
+        Workbook workbook = exporter.toWorkbook();
+        this.getClass();
+        File filename = new File("hello.xlsx");
+        workbook.write(new FileOutputStream(filename));
+    }
+
+    protected List<User> getData() {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("jacky");
+        user.setTelephone("14210064865");
+        List<User> result = new LinkedList<>();
+        result.add(user);
+        return result;
+
+    }
 }
